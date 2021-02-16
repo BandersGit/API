8d52cefd6af6edf905d778f187e3ef9717d04143
BandersGit <andy.i.flaskan@gmail.com>
2021-02-16 10:07:01 +0100

    Inital Commit

---
+++ API/Program.cs
@@ -0,0 +1,15 @@
+﻿using Internal;
+using System;
+
+namespace API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
